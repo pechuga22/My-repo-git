6cf4ebb83c7dcaa65da2ddd41ca6c0130ef97be3
Sala 310 <sala310@poliacademico.edu.loc>
2019-06-12 12:38:02 -0500

    actualizado

---
+++ retosJavascript/6-reto.js
@@ -0,0 +1,17 @@
+// Declarations
+var STUDLYCAPVAR;
+var StUdLyCapVaR;
+var properCamelCase;
+var TitleCaseOver;
+var studlyCapVar;
+var titleCaseOver;
+var PRoperCAmelCAse;
+var tITLEcASEoVER;
+
+// Assignments
+STUDLYCAPVAR = 10;
+studlyCapVar =10;
+PRoperCAmelCAse = "A String";
+properCamelCase ="A String";
+tITLEcASEoVER = 9000;
+titleCaseOver =9000;
