dd11d6ea40b87deaea8670595b1bf451041b993e
pechuga22 <iefaz.santiagovalle@gmail.com>
2019-06-19 11:52:28 -0500

    actualizado

---
+++ retosJavascript/78-reto.js
@@ -0,0 +1,19 @@
+// Example
+var ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"]
+  };
+  
+  // Only change code below this line.
+  
+  var myDog = {
+    "name": "Chalo",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["todos"]
+    
+    
+    
+  };
