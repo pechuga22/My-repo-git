4a5471004658b641d078901c824c29658a5cea0b
pechuga22 <iefaz.santiagovalle@gmail.com>
2019-06-21 00:44:02 -0500

    Actualizado

---
+++ retosJavascript/86-reto.js
@@ -0,0 +1,28 @@
+var myMusic = [
+    {
+      "artist": "Billy Joel" , 
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [ 
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    // Add record here{
+   {
+        "artist": "Billy Joel" , 
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [ 
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+   }
+  
+  
+    
+  ];
