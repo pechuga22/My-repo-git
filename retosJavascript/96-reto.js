9f3bee9fea56fa19c635ffc2d170a154ac6c2464
pechuga22 <iefaz.santiagovalle@gmail.com>
2019-06-21 02:04:23 -0500

    Actualizado

---
+++ retosJavascript/96-reto.js
@@ -0,0 +1,16 @@
+// Example
+var ourArr = [ 9, 10, 11, 12];
+var ourTotal = 0;
+
+for (var i = 0; i < ourArr.length; i++) {
+  ourTotal += ourArr[i];
+}
+var total=0;
+// Setup
+var myArr = [ 2, 3, 4, 5, 6];
+
+  for(i=0;i<5;i++){
+    total +=myArr[i];
+  }
+// Only change code below this line
+
